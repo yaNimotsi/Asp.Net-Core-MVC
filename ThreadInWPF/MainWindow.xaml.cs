d44757915ae3e73a4654039484d8c0f374878c14
Иван Истомин <nimotsiky@gmail.com>
2022-03-27 22:12:54 +0300

    Задание 1.1

--- ThreadInWPF/MainWindow.xaml.cs
+++ ThreadInWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ThreadInWPF
 {
@@ -23,6 +25,42 @@ namespace ThreadInWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Run();
+        }
+
+        private void Run()
+        {
+            var threadFibonachi = new Thread(TextBlockUpdateInThread)
+            {
+                Name = "TextBlockTextUpdateThread"
+            };
+            threadFibonachi.Start();
+        }
+
+        private void TextBlockUpdateInThread()
+        {
+            var fibonachiNumber = 0;
+            var summand = 1;
+            var oldSummand = fibonachiNumber;
+
+            while(fibonachiNumber >= 0)
+            {
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                {
+                    MyTextBlock.Text = fibonachiNumber.ToString();
+                }));
+
+                Thread.Sleep(500);
+
+                oldSummand = fibonachiNumber;
+                fibonachiNumber += summand;
+                summand = oldSummand;
+
+            }
         }
     }
 }
