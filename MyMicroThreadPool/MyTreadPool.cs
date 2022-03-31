d744ab52754116931499033830c2f9b6a5f9b014
Иван Истомин <nimotsiky@gmail.com>
2022-03-31 18:42:09 +0300

    Промежуточный результат

---
+++ MyMicroThreadPool/MyTreadPool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMicroThreadPool
+{
+    internal class MyTreadPool
+    {
+        private int MinItem = 3;
+
+        private ConcurrentQueue<WorkItemThreadPool> _queue = new ConcurrentQueue<WorkItemThreadPool>();
+        private List<ItemThreadPool> _taskList = new List<ItemThreadPool>();
+        public MyTreadPool()
+        {
+            Initializer();
+        }
+
+        /// <summary>
+        /// Первичное создание и настройка пула
+        /// </summary>
+        private void Initializer()
+        {
+
+        }
+    }
+}
---
+++ MyMicroThreadPool/WorkItemThreadPool.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMicroThreadPool
+{
+    internal class WorkItemThreadPool
+    {
+        public int id;
+        public bool isActive;
+    }
+}
