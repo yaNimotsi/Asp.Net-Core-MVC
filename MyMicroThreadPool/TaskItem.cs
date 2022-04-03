5a3810404c3945a10c24e3384420da90d2337a7c
yaNimotsi <yanimotsi@yanmdex.ru>
2022-04-03 14:28:08 +0300

    Промежуточный результат

---
+++ MyMicroThreadPool/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace MyMicroThreadPool
+{
+    public class TaskItem
+    {
+        public TaskInfo taskInfo;
+        public Thread thread;
+        public bool isFree;
+        public Exception taskException;
+    }
+}
