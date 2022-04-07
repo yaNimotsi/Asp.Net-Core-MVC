faa0767c8fef60491e291097935aee1f91a64d3e
Иван Истомин <nimotsiky@gmail.com>
2022-04-07 08:53:01 +0300

    Подготовка к реализации. Принял, что модели данных из внешних источников известны изначально.

---
+++ AdapterPattern/ExternalJsonModel/Model1.cs
@@ -0,0 +1,12 @@
+﻿namespace AdapterPattern
+{
+    public class Model1
+    {
+        public int ItemId { get; set; }
+        public string Company { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Unit { get; set; }
+        public string Article { get; set; }
+    }
+}
---
+++ AdapterPattern/ExternalModelToClientUnitOfTradeAdaptre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class ExternalModelToClientUnitOfTradeAdaptre : ClientUnitOfTrade
+    {
+        public JsonDocument document;
+
+        public ExternalModelToClientUnitOfTradeAdaptre(JsonDocument doc)
+        {
+            document = doc;
+        }
+        //Если я правильно понимаю, то тут необходимо выполнить мапинг 
+        //полей на модель ClientUnitOfTrade
+    }
+}
